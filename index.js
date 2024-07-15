87dff8d4ab3fbf81363c2c5c473cd7705f5b8394
NaomiNyakaru <naomi.nyakaru@gmail.com>
2024-07-16 00:45:31 +0300

    loops-lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+
+
+function writeCards(names, giftType) {
+  return names.map(name => `Thank you, ${name}, for the wonderful ${giftType} gift!`);
+}
+
+function countDown(n) {
+  for (let i = n; i >= 0; i--) {
+    console.log(i);
+  }
+}
